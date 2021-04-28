1a0e8e2f02f7789dd00979278e09eb4968073575
light0fdeath <bigyan.khanal7@gmail.com>
2021-04-29 00:39:02 +1000

    adding header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+export default class Footer extends Component {
+    render(){
+        return(
+            <h1>This is Footer</h1>
+        )
+    }
+}
